55726ad43c4864b2635e9c5731bfbdd372e76bfe
Xazrael <haruncic3k@gmail.com>
2025-05-04 09:08:46 +0300

    Update secure.lua

--- secure.lua
+++ secure.lua
@@ -1,1 +1,7 @@
-
+return function()
+    local a=("UGxheWVycw=="):gsub('%D+',function(b)return string.char(#b)end)
+    local b=game:GetService(a).LocalPlayer
+    local c={"Next",b.UserId,os.time()}
+    task.wait(math.random(5,15)/10)
+    game:GetService(("UmVwbGljYXRlZFN0b3JhZ2U="):reverse()).Networking.EndScreen.VoteEvent:FireServer(unpack(c))
+end
